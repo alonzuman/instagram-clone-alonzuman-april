ef0a7124818000a3d29d9b327cd045bfcbbc46a2
alon zuman <alonzuman7@gmail.com>
2020-04-01 12:27:27 +0300

    Added validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  validates :photo, presence: true
+  validates :description, presence: true
   belongs_to :user, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_one_attached :photo
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_one_attached :photo
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  validates :username, presence: true, uniqueness: true
+  validates :full_name, presence: true
+  validates :photo, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
