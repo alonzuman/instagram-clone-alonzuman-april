881c58a86c499fa697ed03683bd4641cb9295303
alon zuman <alonzuman7@gmail.com>
2020-04-01 20:41:20 +0300

    a ton of stuff

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates :user, uniqueness: true, presence: true
-  validates :post, uniqueness: true, presence: true
+  validates :user, presence: true
+  validates :post, presence: true
 end
