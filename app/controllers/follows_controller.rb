4bfa002a3f811099abc7e3bd7f93164564cb587b
alon zuman <alonzuman7@gmail.com>
2020-04-02 22:14:46 +0300

    added followings

---
+++ app/controllers/follows_controller.rb
@@ -0,0 +1,17 @@
+class FollowsController < ApplicationController
+  def new
+    @follow = Follow.new
+    @follow.follower_id = current_user.id
+    @follow.followed_user_id = params[:user_id]
+    @follow.save
+
+    redirect_to user_path(params[:user_id])
+  end
+
+  def destroy
+    @follow = Follow.find_by(follower_id: current_user.id, followed_user_id: params[:id]) 
+    @follow.destroy
+    
+    redirect_to user_path(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :users, only: [:show]
+  resources :users, only: [:show] do 
+    resources :follows, only: [:new, :create]
+  end
   resources :posts, only: [:index, :show, :new, :create, :destroy] do 
     resources :comments, only: [:new, :create]
     resources :likes, only: [:new, :create]
   end
   resources :comments, only: [:destroy]
   resources :likes, only: [:destroy]
+  resources :follows, only: [:destroy]
 end
